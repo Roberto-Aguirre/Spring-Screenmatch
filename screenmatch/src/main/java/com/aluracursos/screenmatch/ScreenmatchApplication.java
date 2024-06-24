0f40517d33395810f3fc6113c61d6702c97c17e6
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2024-06-23 22:50:59 -0600

    Branch nueva y hola mundo

--- screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,20 @@
 package com.aluracursos.screenmatch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
-		SpringApplication.run(ScreenmatchApplication.class, args);
+		// SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hola mundo desde Spring");
+		// throw new UnsupportedOperationException("Unimplemented method 'run'");
 	}
 
 }
