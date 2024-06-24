8cde18801f2846668ffa8df9754494ad2492d0fa
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2024-06-23 23:17:04 -0600

    Prueba http y ingreso de dependencia maven

--- screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -4,16 +4,22 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.aluracursos.screenmatch.services.ConsumoAPI;
+
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
-		// SpringApplication.run(ScreenmatchApplication.class, args);
+		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hola mundo desde Spring");
+		var consumoAPI = new ConsumoAPI();
+		// var json = consumoAPI.obtenerDatos("http://omdbapi.com/?apikey=57bedfc9&t=game+of+thrones");
+		var json = consumoAPI.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+		
 		// throw new UnsupportedOperationException("Unimplemented method 'run'");
 	}
 
