755fbc4ad5086a37effe19da371b25f8731a0190
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2024-06-24 23:26:12 -0600

    Se muestran las temporadas y los nombres de los episodios

--- screenmatch/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import com.aluracursos.screenmatch.modelos.DatosEpisodio;
 import com.aluracursos.screenmatch.modelos.DatosSerie;
 import com.aluracursos.screenmatch.modelos.DatosTemporada;
 import com.aluracursos.screenmatch.services.ConsumoAPI;
@@ -18,24 +19,38 @@ public class Principal {
     private ConvierteDatos conversor = new ConvierteDatos();
 
     public void mostrarMenu() {
-        //Ingreso de Input
+        // Ingreso de Input
         System.out.println("Por favor escribe el nombre de la serie que deseas buscar");
         var nombreSerie = teclado.nextLine();
         var nombreSerieFix = nombreSerie.replace(" ", "+");
-        //Busca los datos de las temporadas.
-        var json = consumoAPI.obtenerDatos(URL_BASE+API_KEY+ nombreSerieFix );
+        // Busca los datos de las temporadas.
+        var json = consumoAPI.obtenerDatos(URL_BASE + API_KEY + nombreSerieFix);
         var datos = conversor.obtenerDatos(json, DatosSerie.class);
-        //Inicializar 
+        // Inicializar
         List<DatosTemporada> temporadas = new ArrayList<>();
-		for (int i = 1; i < datos.totaltemporadas(); i++) {
-			
-			var temporada = consumoAPI.obtenerDatos(URL_BASE+API_KEY+nombreSerieFix+"&Season="+i);
-            //http://omdbapi.com/? apikey=57bedfc9&t=game+of+thrones&Season=1
-            System.out.println(temporada);
-			var datosTemporada = conversor.obtenerDatos(temporada, DatosTemporada.class);
-			temporadas.add(datosTemporada);
-		}
-		
-		temporadas.forEach(System.out::println);
+        for (int i = 1; i <= datos.totaltemporadas(); i++) {
+
+            var temporada = consumoAPI.obtenerDatos(URL_BASE + API_KEY + nombreSerieFix + "&Season=" + i);
+            // System.out.println(temporada);
+            var datosTemporada = conversor.obtenerDatos(temporada, DatosTemporada.class);
+            temporadas.add(datosTemporada);
+        }
+
+        // temporadas.forEach(System.out::println);
+        System.out.println(datos.totaltemporadas());
+        System.out.println(temporadas.size());
+
+        for (int i = 0; i < datos.totaltemporadas(); i++) {
+            List<DatosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+            System.out.println("Temporada: "+temporadas.get(i).numeroTemporada());
+            
+            for (int j = 0; j < episodiosTemporada.size(); j++) {
+                System.out.println(episodiosTemporada.get(j).titulo());
+                // DEBUG
+                // System.out.println(i + " " + j);
+            }
+        }
+
     }
+
 }
